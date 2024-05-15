d555a57a45f08361cf9c7214673ad3ebb7504cba
alrayjr <59267280+alrayjr@users.noreply.github.com>
2024-05-14 21:17:50 -0400

    Add files via upload

---
+++ Maths.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package maths;
+
+/**
+ *
+ * @author epicb
+ */
+public class Maths {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.tan(90));
+        
+        System.out.println(Math.max(4,7));
+        System.out.println(Math.sqrt(16));
+        
+    }
+    
+}
